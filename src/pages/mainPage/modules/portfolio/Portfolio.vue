ba48a3616cae35f109f1ebc1a4e32367fb529f7b
Gamspi <niko.tatt007@gmail.com>
2023-06-21 19:25:05 +0700

    feature/  добавлен блок портфолио

---
+++ src/pages/mainPage/modules/portfolio/Portfolio.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="portfolio" />
+</template>
+
+<script lang='ts'>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Portfolio',
+  setup () {
+    return {}
+  }
+})
+</script>

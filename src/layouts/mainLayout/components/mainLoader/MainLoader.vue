806e5aaba26437fd40ecc220ed70245e2d6eba44
Gamspi <niko.tatt007@gmail.com>
2023-07-01 03:03:36 +0700

    feature/ переделан главный лоадер

--- src/layouts/mainLayout/components/mainLoader/MainLoader.vue
+++ src/layouts/mainLayout/components/mainLoader/MainLoader.vue
@@ -1,39 +1,43 @@
 <template>
   <Teleport to="#main-loader">
-    <Transition name="main-loader">
+    <Transition
+      name="main-loader"
+      duration="1000"
+    >
       <div
         v-if="isShow || isShowComponent"
         class="main-loader"
       >
-        <div class="main-loader__balls">
-          <div class="main-loader__ball">
-            <Ball
-              color="blue"
-              is-animated
-              animation-number="1"
-            />
+        <div class="main-loader__container">
+          <div class="main-loader__balls">
+            <div class="main-loader__ball">
+              <Ball
+                color="blue"
+                is-animated
+                animation-number="1"
+              />
+            </div>
+            <div class="main-loader__ball">
+              <Ball
+                color="blue"
+                is-animated
+                animation-number="2"
+              />
+            </div>
+            <div class="main-loader__ball">
+              <Ball
+                is-animated
+                animation-number="3"
+              />
+            </div>
+            <div class="main-loader__ball">
+              <Ball
+                is-animated
+                animation-number="4"
+              />
+            </div>
           </div>
-          <div class="main-loader__ball">
-            <Ball
-              color="blue"
-              is-animated
-              animation-number="2"
-            />
-          </div>
-          <div class="main-loader__ball">
-            <Ball
-              is-animated
-              animation-number="3"
-            />
-          </div>
-          <div class="main-loader__ball">
-            <Ball
-              is-animated
-              animation-number="4"
-            />
-          </div>
-        </div>
-        <div class="main-loader__wrapper">
+
           <div class="main-loader__body">
             <img
               src="../../../../assets/icons/mainLoader/loaderLogo.svg"
@@ -47,12 +51,10 @@
               <h4 class="main-loader__sub-title">
                 Creative
               </h4>
+              <p class="main-loader__description">
+                will help to reach your goals.
+              </p>
             </div>
-          </div>
-          <div class="main-loader__footer">
-            <p class="main-loader__description">
-              will help to reach your goals.
-            </p>
           </div>
         </div>
       </div>
@@ -60,7 +62,7 @@
   </Teleport>
 </template>
 
-<script lang='ts'>
+<script lang="ts">
 import { defineComponent } from 'vue'
 import './style.scss'
 import Ball from 'components/ball/Ball.vue'

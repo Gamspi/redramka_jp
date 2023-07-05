00312d72c7fe9454981ba514e7fd085c21e30406
Gamspi <niko.tatt007@gmail.com>
2023-07-05 22:41:55 +0700

    feature/ измененыа позиции шаров

--- src/components/balls/Balls.vue
+++ src/components/balls/Balls.vue
@@ -1,38 +1,41 @@
 <template>
   <div class="balls">
-    <div class="balls__item">
-      <Ball
-        color="blue"
-        is-animated
-        animation-number="1"
-      />
-    </div>
-    <div class="balls__item">
-      <Ball
-        color="blue"
-        is-animated
-        animation-number="2"
-      />
-    </div>
-    <div class="balls__item">
-      <Ball
-        is-animated
-        animation-number="3"
-      />
-    </div>
-    <div class="balls__item">
-      <Ball
-        is-animated
-        animation-number="4"
-      />
+    <div class="balls__container container">
+      <div class="balls__item">
+        <Ball
+          color="blue"
+          is-animated
+          animation-number="1"
+        />
+      </div>
+      <div class="balls__item">
+        <Ball
+          color="blue"
+          is-animated
+          animation-number="2"
+        />
+      </div>
+      <div class="balls__item">
+        <Ball
+          is-animated
+          animation-number="3"
+        />
+      </div>
+      <div class="balls__item">
+        <Ball
+          is-animated
+          animation-number="4"
+        />
+      </div>
     </div>
   </div>
 </template>
 
-<script lang='ts'>
+<script lang="ts">
 import { defineComponent } from 'vue'
 import Ball from 'components/ball/Ball.vue'
 import './style.scss'
+
 export default defineComponent({
   name: 'Balls',
   components: { Ball },

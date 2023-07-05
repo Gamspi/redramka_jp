0c28c528db0a7e741a126091209267027390f7a7
Gamspi <niko.tatt007@gmail.com>
2023-07-05 20:13:04 +0700

    feature/ функционал файлового инпута

--- src/components/filePicker/controller.ts
+++ src/components/filePicker/controller.ts
@@ -1,19 +1,28 @@
+import { ref } from 'vue'
+
 type Args = {
   emit: (event: string, ...args: any[]) => void
 }
 
 export const useController = ({ emit }: Args) => {
+  const files = ref<File[]>([])
+  const handelRemoveFile = (file: File) => {
+    files.value = files.value.filter(item => item !== file)
+    emit('update:modelValue', files.value)
+  }
   const handelChangeInput = (e: InputEvent) => {
     const target = e.target as HTMLInputElement
-    const result = []
+
     if (target.files) {
       for (let i = 0; i < target.files.length; i++) {
-        result.push(target.files[i])
+        files.value.push(target.files[i])
+        console.log(target.files[i])
       }
     }
-    emit('update:modelValue', result)
+    emit('update:modelValue', files.value)
   }
   return {
+    handelRemoveFile,
     handelChangeInput
   }
 }
